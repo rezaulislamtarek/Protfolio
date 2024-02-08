4997e6e34fc29d65b8da1787e44bd0e31349f19f
rezaulislam <mrezaulislam.t@gmail.com>
2024-02-08 19:12:42 +0600

    Initial Commit

---
+++ Sources/Protfolio/ProtfolioCoordiantor.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Rezaul Islam on 8/2/24.
+//
+
+import SwiftUI
+
+struct ProtfolioCoordiantor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProtfolioCoordiantor()
+}
